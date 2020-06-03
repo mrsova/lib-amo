03776a1f92859bff902c1387df1195081768c0b1
mrsovad <serov-pa@yandex.ru>
2020-06-03 17:52:30 +0300

    fix

--- ParamsInterface.php
+++ ParamsInterface.php
@@ -5,4 +5,12 @@ namespace libAmo;
 interface ParamsInterface
 {
     public function addAuth($key, $value);
+
+    public function addDomain($value);
+
+    public function addLogin($value);
+
+    public function addApiKey($value);
+
+    public function addAccessToken($value);
 }
